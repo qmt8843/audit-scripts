4f51269d28311250609d6db5a374149eadc60861
qmt8843 <qmt8843@rit.edu>
2024-11-17 15:15:46 -0500

    Added try catch

--- ad-check.ps1
+++ ad-check.ps1
@@ -1,30 +1,48 @@
-$ldapsigningpath = "HKLM:\SYSTEM\CurrentControlSet\Services\NTDS\Parameters"
-$ldapsigningname = "LDAPServerIntegrity"
-$ldapsigningoutcome = Get-ItemProperty -Path $ldapsigningpath -Name $ldapsigningname | Select-Object -ExpandProperty $ldapsigningname
-if (%ldapsigningoutcome){
-    Write-Output "LDAP Signing output: $ldapsigningoutcome`r`n"
+try {
+    $ldapsigningpath = "HKLM:\SYSTEM\CurrentControlSet\Services\NTDS\Parameters"
+    $ldapsigningname = "LDAPServerIntegrity"
+    $ldapsigningoutcome = Get-ItemProperty -Path $ldapsigningpath -Name $ldapsigningname | Select-Object -ExpandProperty $ldapsigningname
+    if (%ldapsigningoutcome) {
+        Write-Output "LDAP Signing output: $ldapsigningoutcome`r`n"
+    }
+    #1 means none
+    #2 means require signing
 }
-#1 means none
-#2 means require signing
+catch {
+    Write-Output "Error auditing LDAP Signing"
+}
 
-$auditkerbauthsrv = auditpol /get /subcategory:"Kerberos Authentication Service" | FindStr "Kerberos"
-if (%auditkerbauthsrv){
-    Write-Output "Audit Kerberos Authentication Service output: $auditkerbauthsrv`r`n"
-}
-#Default is success
-#Should be set to success & failure
 
-$auditkerbsrtvticket = auditpol /get /subcategory:"Kerberos Service Ticket Operations"
-if (%auditkerbsrtvticket){
-    Write-Output "Kerberos Service Ticket Operations output: $auditkerbsrtvticket`r`n"
+try {
+    $auditkerbauthsrv = auditpol /get /subcategory:"Kerberos Authentication Service" | FindStr "Kerberos"
+    if (%auditkerbauthsrv) {
+        Write-Output "Audit Kerberos Authentication Service output: $auditkerbauthsrv`r`n"
+    }
+    #Default is success
+    #Should be set to success & failure
+} catch {
+    Write-Output "Error auditing Audit Kerberos Authentication Service"
 }
-#Default is success
-#Should be set to success & failure
 
-$kerbencryptionpath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\Kerberos\Parameters"
-$kerbencryptionname = "SupportedEncryptionTypes"
-$kerbencryptionoutput = Get-ItemProperty -Path $kerbencryptionpath -Name $kerbencryptionname | Select-Object -ExpandProperty $kerbencryptionname
-if (%kerbencryptionoutput){
-    Write-Output "Kerberos Encryption Support: $kerbencryptionoutput`r`n"
+try {
+    $auditkerbsrtvticket = auditpol /get /subcategory:"Kerberos Service Ticket Operations"
+    if (%auditkerbsrtvticket) {
+        Write-Output "Audit Kerberos Service Ticket Operations output: $auditkerbsrtvticket`r`n"
+    }
+    #Default is success
+    #Should be set to success & failure
+} catch {
+    Write-Output "Error auditing Audit Kerberos Service Ticket Operations"
 }
-#Should be 2147483640
+
+try {
+    $kerbencryptionpath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\Kerberos\Parameters"
+    $kerbencryptionname = "SupportedEncryptionTypes"
+    $kerbencryptionoutput = Get-ItemProperty -Path $kerbencryptionpath -Name $kerbencryptionname | Select-Object -ExpandProperty $kerbencryptionname
+    if (%kerbencryptionoutput) {
+        Write-Output "Kerberos Encryption Support: $kerbencryptionoutput`r`n"
+    }
+    #Should be 2147483640
+} catch {
+    Write-Output "Error auditing Kerberos Encryption Support"
+}
