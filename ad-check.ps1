4c8778eb92960cc6e974ddcbe24ed4aabe351120
qmt8843 <qmt8843@rit.edu>
2024-11-17 14:33:33 -0500

    Added some new lines

--- ad-check.ps1
+++ ad-check.ps1
@@ -1,22 +1,22 @@
 $ldapsigningpath = "HKLM:\SYSTEM\CurrentControlSet\Services\NTDS\Parameters"
 $ldapsigningname = "LDAPServerIntegrity"
 $ldapsigningoutcome = Get-ItemProperty -Path $ldapsigningpath -Name $ldapsigningname | Select-Object -ExpandProperty $ldapsigningname
-Write-Output "LDAP Signing output: $ldapsigningoutcome"
+Write-Output "LDAP Signing output: $ldapsigningoutcome`r`n"
 #1 means none
 #2 means require signing
 
 $auditkerbauthsrv = auditpol /get /subcategory:"Kerberos Authentication Service" | FindStr "Kerberos"
-Write-Output "Audit Kerberos Authentication Service output: $auditkerbauthsrv"
+Write-Output "Audit Kerberos Authentication Service output: $auditkerbauthsrv`r`n"
 #Default is success
 #Should be set to success & failure
 
 $auditkerbsrtvticket = auditpol /get /subcategory:"Kerberos Service Ticket Operations"
-Write-Output "Kerberos Service Ticket Operations output: $auditkerbsrtvticket"
+Write-Output "Kerberos Service Ticket Operations output: $auditkerbsrtvticket`r`n"
 #Default is success
 #Should be set to success & failure
 
-$kerbencryptionpath = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\Kerberos\Parameters"
+$kerbencryptionpath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\Kerberos\Parameters"
 $kerbencryptionname = "SupportedEncryptionTypes"
 $kerbencryptionoutput = Get-ItemProperty -Path $kerbencryptionpath -Name $kerbencryptionname | Select-Object -ExpandProperty $kerbencryptionname
-Write-Output "Kerberos Encryption Support: $kerbencryptionoutput"
+Write-Output "Kerberos Encryption Support: $kerbencryptionoutput`r`n"
 #Should be 2147483640
