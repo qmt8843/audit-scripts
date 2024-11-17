5c9ec7a70ca935c4472a75465593855764e04d79
qmt8843 <qmt8843@rit.edu>
2024-11-17 16:55:51 -0500

    Updated encrypt secure channel to only get requiresignorseal

--- ad-check.ps1
+++ ad-check.ps1
@@ -80,7 +80,7 @@ try {
     if (Test-Path $encryptsecurechannelpath) {
         $encryptsecurechanneloutcome = Get-ItemProperty -Path $encryptsecurechannelpath -Name $encryptsecurechannelname | Select-Object -ExpandProperty $encryptsecurechannelame
         if ($encryptsecurechanneloutcome ) {
-            Write-Output "Digitally encrypt or sign secure channel data (always) output: $encryptsecurechanneloutcome`r`n"
+            Write-Output "Digitally encrypt or sign secure channel data (always) output: $encryptsecurechanneloutcome.RequireSignOrSeal`r`n"
         }
         #Should be 1 (enabled)
         #Path doesn't exist or error means it is disabled
